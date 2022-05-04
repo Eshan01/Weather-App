66af8b7e92f19cacf564adad2c92e16e0fe784fb
Eshan01 <30386331+Eshan01@users.noreply.github.com>
2022-05-05 00:07:14 +0530

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -4,7 +4,7 @@ const key='IOXlgKplDGYoSbU2HkHiCLKe1mmDU7PV';
 const getCity= async (city)=>{
     const base='https://dataservice.accuweather.com/locations/v1/cities/search';
     const query=`?apikey=${key}&q=${city}`;
-    const response= await fetch(base+query);
+    const response= await fetch(base+query,{mode:'no-cors'});
     const data=await response.json()
     return data[0];
 };
@@ -12,7 +12,7 @@ const getCity= async (city)=>{
 const getWeather = async (locationKey)=>{
     const base=`https://dataservice.accuweather.com/currentconditions/v1/${locationKey}`;
     const query=`?apikey=${key}`
-    const response =await fetch(base+query);
+    const response =await fetch(base+query,{mode:'no-cors'});
     const data= await response.json();
 
     return data;
